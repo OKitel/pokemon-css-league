234eb47543e1daeaeea9f238ab7129e4934addf1
OKitel <kitelolga@gmail.com>
2023-07-01 18:35:15 +0200

    init: setup project

---
+++ rs-css/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext]',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Minesweeper',
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: './public' }],
+        }),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),
+    },
+};
