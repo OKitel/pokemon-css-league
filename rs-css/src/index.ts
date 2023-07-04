460f2b8074cdf9840052f9345783c1a6ba5f471e
OKitel <kitelolga@gmail.com>
2023-07-04 04:43:11 +0200

    feat: add render methods

--- rs-css/src/index.ts
+++ rs-css/src/index.ts
@@ -1,41 +1,113 @@
 import './sass/styles.scss';
-import charmanderImage from './assets/charmander.png';
-import bulbasaurImage from './assets/bulbasaur.png';
-import pikachuImage from './assets/pikachu.png';
-import squirtleImage from './assets/squirtle.png';
+import { Level, LevelJson } from './types/level';
+import Pokemon from './types/pokemon';
+import { Answer } from './types/answer';
 import { initCssEditor, initHtmlEditor } from './scripts/editor';
 import initMenu from './scripts/menu';
 import initModal from './scripts/modal';
+import * as dataBase from './levels.json';
+import { getPokemonsByIds } from './repo/pokemonRepository';
+import createHeader from './scripts/header';
+import createFooter from './scripts/footer';
+import createMain from './scripts/main';
 
-const pokemonsField = document.querySelector('.pokemons');
-const charmander = document.createElement('img');
-charmander.src = charmanderImage as string;
-charmander.classList.add('pokemon');
-pokemonsField?.appendChild(charmander);
+interface LevelsData {
+    levels: LevelJson[];
+}
 
-const bulbasaur = document.createElement('img');
-bulbasaur.src = bulbasaurImage as string;
-bulbasaur.classList.add('pokemon');
-pokemonsField?.appendChild(bulbasaur);
+alert(
+    'Уважаемые проверяющие! Я еще активно доделываю работу, поэтому буду очень признателна, если вы проверите мою работу не ранее 05.07.2023'
+);
 
-const squirtle = document.createElement('img');
-squirtle.src = squirtleImage as string;
-squirtle.classList.add('pokemon');
-pokemonsField?.appendChild(squirtle);
+const jsonData = dataBase as LevelsData;
 
-const pikachu = document.createElement('img');
-pikachu.src = pikachuImage as string;
-pikachu.classList.add('pokemon');
-pokemonsField?.appendChild(pikachu);
+const createLevels = (data: LevelsData): Level[] => {
+    return data.levels.map((level) => {
+        const pokemonsters: Pokemon[] = getPokemonsByIds(level.pokemons);
+        const corAns: Answer[] = level.correctAnswers.map(({ css, pokemons }) => ({
+            css,
+            pokemons: getPokemonsByIds(pokemons),
+        }));
+        const wrongAns: Answer[] | undefined = level.wrongAnswers?.map(({ css, pokemons }) => ({
+            css,
+            pokemons: getPokemonsByIds(pokemons),
+        }));
+        return { ...level, pokemons: pokemonsters, correctAnswers: corAns, wrongAnswers: wrongAns };
+    });
+};
+
+const levels = createLevels(jsonData);
+
+const createView = (): void => {
+    const root = document.getElementById('root');
+
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    const header = createHeader();
+    container.appendChild(header);
+
+    const main = createMain();
+    const footer = createFooter();
+
+    const backgroundContainer = document.createElement('div');
+    backgroundContainer.className = 'background';
+    backgroundContainer.appendChild(main);
+    backgroundContainer.appendChild(footer);
+    container.appendChild(backgroundContainer);
+    if (root) {
+        root.appendChild(container);
+    }
+
+    initMenu();
+    initModal();
+};
+
+createView();
 
 const editorField = document.querySelector<HTMLElement>('.editor');
 if (editorField) {
     const cssEditorField = document.getElementById('css-editor');
     if (cssEditorField) initCssEditor(cssEditorField);
-
-    const htmlViewerField = document.getElementById('html-viewer');
-    if (htmlViewerField) initHtmlEditor(htmlViewerField);
 }
 
-initMenu();
-initModal();
+const renderTitle = (level: Level): void => {
+    const main = document.querySelector('.main');
+    const levelTitle = document.createElement('h2');
+    levelTitle.className = 'task-title';
+    levelTitle.innerText = level.title;
+    if (main) {
+        main.prepend(levelTitle);
+    }
+};
+
+const renderHtmlViewer = (level: Level): void => {
+    const htmlViewerField = document.getElementById('html-viewer');
+    if (htmlViewerField) initHtmlEditor(htmlViewerField, level.html);
+};
+
+const renderPokemons = (level: Level): void => {
+    const pokemonsField = document.querySelector('.pokemons');
+    const correctPokemonIds = level.correctAnswers.flatMap((ans) => ans.pokemons.map((p) => p.id));
+    if (pokemonsField) {
+        const { pokemons } = level;
+        pokemons.forEach((p: Pokemon) => {
+            const pImg = document.createElement('img');
+            pImg.src = p.src;
+            pImg.classList.add('pokemon');
+
+            if (correctPokemonIds.includes(p.id)) {
+                pImg.classList.add('active-image');
+            }
+            pokemonsField.appendChild(pImg);
+        });
+    }
+};
+
+const render = (level: Level): void => {
+    renderTitle(level);
+    renderHtmlViewer(level);
+    renderPokemons(level);
+};
+
+render(levels[0]);
