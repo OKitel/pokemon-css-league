9360c647f23ab9b58fc38e850dff7ce9d58faf84
OKitel <kitelolga@gmail.com>
2023-07-04 04:39:13 +0200

    feat: add pokemon class

--- rs-css/src/types/pokemon.ts
+++ rs-css/src/types/pokemon.ts
@@ -1,8 +1,42 @@
-export interface Pokemon {
+interface IPokemon {
     id: number;
     name: string;
     src: string;
-    tooltip: () => string;
     attrId?: string;
     attrClass?: string;
 }
+
+class Pokemon implements IPokemon {
+    public id: number;
+
+    public name: string;
+
+    public src: string;
+
+    public attrId?: string;
+
+    public attrClass?: string;
+
+    constructor(p: IPokemon) {
+        this.id = p.id;
+        this.name = p.name;
+        this.src = p.src;
+        this.attrId = p.attrId;
+        this.attrClass = p.attrClass;
+    }
+
+    public tooltip(): string {
+        if (this.attrId) {
+            if (this.attrClass) {
+                return `<${this.name} ${this.attrId} ${this.attrClass}/>`;
+            }
+            return `<${this.name} ${this.attrId} />`;
+        }
+        if (this.attrClass) {
+            return `<${this.name} ${this.attrClass}/>`;
+        }
+        return `<${this.name} />`;
+    }
+}
+
+export default Pokemon;
