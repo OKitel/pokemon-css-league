77f28c404784ac8c64145613bd807d24bd7d7f36
OKitel <kitelolga@gmail.com>
2023-07-06 03:33:16 +0200

    feat: add Enter button

--- rs-css/src/scripts/main.ts
+++ rs-css/src/scripts/main.ts
@@ -1,3 +1,6 @@
+import { checkAnswer } from './level';
+
+// eslint-disable-next-line max-lines-per-function
 const createMain = (): HTMLElement => {
     const main = document.createElement('main');
     main.className = 'main';
@@ -10,8 +13,17 @@ const createMain = (): HTMLElement => {
     cssEditorHeader.textContent = 'CSS Editor';
     const cssViewerContainer = document.createElement('div');
     cssViewerContainer.className = 'css-viewer__container';
+    const enterBtn = document.createElement('button');
+    enterBtn.className = 'enter-btn btn';
+    enterBtn.textContent = 'Enter';
+    enterBtn.addEventListener('click', () => {
+        const userAnswer = '';
+        checkAnswer(userAnswer);
+    });
+
     cssEditorDiv.appendChild(cssEditorHeader);
     cssEditorDiv.appendChild(cssViewerContainer);
+    cssEditorDiv.appendChild(enterBtn);
 
     const htmlViewerDiv = document.createElement('div');
     htmlViewerDiv.id = 'html-viewer';
