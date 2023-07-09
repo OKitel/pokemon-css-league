1ee9a6f5cdac6009de49d99b5b1bb73f519dc909
OKitel <kitelolga@gmail.com>
2023-07-09 21:39:27 +0200

    refactor: add types in footer module

--- rs-css/src/modules/footer.ts
+++ rs-css/src/modules/footer.ts
@@ -1,29 +1,29 @@
 import { GITHUB_LINK, RSS_LINK } from '../consts';
 
 const createFooterView = (): HTMLElement => {
-    const footer = document.createElement('footer');
+    const footer: HTMLElement = document.createElement('footer');
     footer.className = 'footer';
 
-    const githubLink = document.createElement('a');
+    const githubLink: HTMLAnchorElement = document.createElement('a');
     githubLink.className = 'footer__link';
     githubLink.href = GITHUB_LINK;
     githubLink.target = '_blank';
 
-    const githubIcon = document.createElement('div');
+    const githubIcon: HTMLDivElement = document.createElement('div');
     githubIcon.className = 'github-icon';
 
     githubLink.appendChild(githubIcon);
 
-    const year = document.createElement('h3');
+    const year: HTMLHeadingElement = document.createElement('h3');
     year.className = 'footer__year';
     year.textContent = '2023';
 
-    const rssLink = document.createElement('a');
+    const rssLink: HTMLAnchorElement = document.createElement('a');
     rssLink.className = 'footer__link';
     rssLink.href = RSS_LINK;
     rssLink.target = '_blank';
 
-    const rssIcon = document.createElement('div');
+    const rssIcon: HTMLDivElement = document.createElement('div');
     rssIcon.className = 'rss-icon';
 
     rssLink.appendChild(rssIcon);
